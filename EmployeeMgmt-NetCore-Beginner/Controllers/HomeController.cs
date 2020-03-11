14267d469b46bc016fb1482f54a81d92b182cfcd
Manish Walde <manish.walde@gmail.com>
2020-03-11 15:24:59 +0530

    added all netcore practice codes

---
+++ EmployeeMgmt-NetCore-Beginner/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{
+    //[Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            //This is called "constructor injection"
+            _employeeRepository = employeeRepository;
+        }
+
+        //[Route("~/Home")]
+        //[Route("~/")]
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployees();
+            return View("~/Views/Home/Index.cshtml", model);
+        }
+
+        //[Route("{id?}")]
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+            };
+            return View(homeDetailsViewModel);
+        }
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
+        }
+    }
+}
---
+++ EmployeeMgmt-NetCore-Beginner/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Property Shortcut "prop"
+        /// </summary>
+        public int Id { get; set; }
+        [Required, MaxLength(50, ErrorMessage ="Name cannot exceed 50 characters.")]
+        public string Name { get; set; }
+        //[EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
+        [Display (Name="Office Email")]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+
+    }
+}
